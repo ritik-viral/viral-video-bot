22848156261ad1ec03aa162ac0f548538a99dbdc
ritik-viral <ritikbwcg2@gmail.com>
2026-06-16 20:49:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from google import genai
 
 GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 PEXELS_API_KEY = os.environ["PEXELS_API_KEY"]
-TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 ARTICLE = """
-PASTE ARTICLE HERE
+G7 Summit Day 1 LIVE: PM Modi holds brief conversation with U.S. President Trump
+Prime Minister Narendra Modi arrived in France on Tuesday to attend the G7 Summit and exchange views with world leaders on key global issues.
+Prime Minister Narenda Modi talks to U.S. President Donald Trump before the plenary session at the G7 summit, on June 16, 2026, in Evian-les-Bains, France. | Photo Credit: AP
+
+Prime Minister Narendra Modi and U.S. President Donald Trump on Tuesday (June 16, 2026) exchanged pleasantries and held a brief conversation at the G7 summit in the French commune of Evian-les-Bains in the first in-person encounter between the two leaders in nearly one-and-a-half years.
+
+
+President Donald Trump on Tuesday (June 16, 2026) said the U.S. will soon be able to reimpose sanctions against Russian oil, at the G7 summit where leaders are seeking to ratchet up pressure against Moscow over its invasion of Ukraine. G7 leaders agreed on Tuesday (June 16, 2026) to intensify pressure on Russia to end more than four years of war against Ukraine, with U.S. President Donald Trump saying Moscow should “make a deal”. 
 """
 
 PROMPT = f"""
@@ -84,16 +89,7 @@ os.system(
     'ffmpeg -y -i video.mp4 -i voice.mp3 -shortest final.mp4'
 )
 
-url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendVideo"
-
-requests.post(
-    url,
-    data={
-        "chat_id": TELEGRAM_CHAT_ID
-    },
-    files={
-        "video": open("final.mp4", "rb")
-    }
-)
-
-print("DONE")
+print("===================================")
+print("VIDEO CREATED SUCCESSFULLY")
+print("File: final.mp4")
+print("===================================")
